eadee793766f8121cb40f93bd772cdd769bbc089
Materix <michalsuder@wp.pl>
2016-05-15 15:49:00 +0200

    Add calculating fitness in generation

--- examples/experiments/labs-emas-lamarckian/generate.js
+++ examples/experiments/labs-emas-lamarckian/generate.js
@@ -11,6 +11,55 @@ var generate = function(parameters){
         individual.energy = BASIC_ENERGY;
         individuals.push(individual);
     }
+
+    individuals.forEach(function (individual) {
+        individual.fitness = fitness(individual);
+    });
     return {individuals : individuals};
+};
+
+
+function fitness(individual) {
+    var length = individual.bytes.length;
+    return (length * length) / (2 * countEnergy(baldwinLocalSearch(individual.bytes)));
+}
+
+function baldwinLocalSearch(bytes) {
+    var length = bytes.length;
+    do {
+        var originalEnergy = countEnergy(bytes);
+        var bestBytes = bytes;
+        var bestEnergy = originalEnergy;
+        for (var i = 0; i < length; ++i) {
+            var bytesCopy = bytes.slice();
+            bytesCopy[i] = -bytesCopy[i];
+            var newEnergy = countEnergy(bytesCopy);
+            if (newEnergy < bestEnergy) {
+                bestEnergy = newEnergy;
+                bestBytes = bytesCopy;
+            }
+        }
+        bytes = bestBytes;
+    } while (bestEnergy < originalEnergy);
+    return bytes;
+}
+
+function countEnergy(bytes) {
+    var length = bytes.length;
+    var sum = 0;
+    for (var interval = 1; interval < length; ++interval) {
+        var correl = countCorrelation(bytes, interval);
+        sum += correl * correl;
+    }
+    return sum;
+}
+
+function countCorrelation(bytes, interval) {
+    var length = bytes.length;
+    var sum = 0;
+    for (var i = 0; i < length - interval; ++i) {
+        sum += bytes[i] * bytes[i + interval];
+    }
+    return sum;
 }
 
