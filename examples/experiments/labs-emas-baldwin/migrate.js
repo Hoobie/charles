1c20ad00878141e48d5ccda1968e7a0228e3e329
Materix <michalsuder@wp.pl>
2016-05-21 21:07:30 +0200

    Protect to emigrate last two individual

--- examples/experiments/labs-emas-baldwin/migrate.js
+++ examples/experiments/labs-emas-baldwin/migrate.js
@@ -1,21 +1,25 @@
 var migrate = function (firstPopulation, secondPopulation, parameters) {
 
-    firstPopulation.individuals.filter(function (individual) {
-        return individual.energy >= MIGRATION_MINIMUM_ENERGY && Math.random() < MIGRATION_PROBABILITY && individual.migrated == false;
-    }).forEach(function (individual) {
-        var index = firstPopulation.individuals.indexOf(individual);
-        var migrant = firstPopulation.individuals.splice(index, 1)[0];
-        migrant.migrated = true;
-        secondPopulation.individuals.push(migrant)
-    });
+    if (firstPopulation.individuals.length > 2) {
+        firstPopulation.individuals.filter(function (individual) {
+            return individual.energy >= MIGRATION_MINIMUM_ENERGY && Math.random() < MIGRATION_PROBABILITY && individual.migrated == false;
+        }).forEach(function (individual) {
+            var index = firstPopulation.individuals.indexOf(individual);
+            var migrant = firstPopulation.individuals.splice(index, 1)[0];
+            migrant.migrated = true;
+            secondPopulation.individuals.push(migrant)
+        });
+    }
 
-    secondPopulation.individuals.filter(function (individual) {
-        return individual.energy >= MIGRATION_MINIMUM_ENERGY && Math.random() < MIGRATION_PROBABILITY && individual.migrated == false;
-    }).forEach(function (individual) {
-        var index = secondPopulation.individuals.indexOf(individual);
-        var migrant = secondPopulation.individuals.splice(index, 1)[0];
-        migrant.migrated = true;
-        firstPopulation.individuals.push(migrant)
-    });
+    if (secondPopulation.individuals.length > 2) {
+        secondPopulation.individuals.filter(function (individual) {
+            return individual.energy >= MIGRATION_MINIMUM_ENERGY && Math.random() < MIGRATION_PROBABILITY && individual.migrated == false;
+        }).forEach(function (individual) {
+            var index = secondPopulation.individuals.indexOf(individual);
+            var migrant = secondPopulation.individuals.splice(index, 1)[0];
+            migrant.migrated = true;
+            firstPopulation.individuals.push(migrant)
+        });
+    }
     return {firstPopulation: firstPopulation, secondPopulation: secondPopulation};
 };
